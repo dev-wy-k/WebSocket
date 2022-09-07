e8e61e71330b00d54c7bc65c0c1165871eccd01a
vlain01 <waiyankyw96@gmail.com>
2022-09-07 21:25:46 +0630

    websocket setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const socket = require('socket.io');
+
+/**____ app setup ____ */
+let app = express();
+
+/**____ server setup ____ */
+let server = app.listen(3000, () => {
+    console.log('project is running on localhost:3000');
+});
+
+/**____ route setup ____ */
+app.get('/', (res, req) => {
+    req.sendFile(__dirname + '/public/index.html');
+})
+
+/**____ socket setup ____ */
+let io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log('Socket Connected !!');
+})
